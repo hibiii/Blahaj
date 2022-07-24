b5316a55e714588afa42787674b1bbc8e424e56d
Hibi <69405847+Hibiii@users.noreply.github.com>
2022-07-24 14:07:21 -0300

    Minor touch ups

--- src/main/java/hibi/blahaj/Common.java
+++ src/main/java/hibi/blahaj/Common.java
@@ -1,8 +1,6 @@
 package hibi.blahaj;
 
 import org.quiltmc.loader.api.ModContainer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.item.Item;
@@ -12,19 +10,17 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Common {
-	public static final Logger LOGGER = LoggerFactory.getLogger("Blahaj");
 
 	public static final Identifier BLAHAJ_ID;
 	public static final Identifier BREAD_ID;
 
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
 		Registry.register(Registry.ITEM, BLAHAJ_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC)));
 		Registry.register(Registry.ITEM, BREAD_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC)));
 	}
 
 	static {
-		BLAHAJ_ID = new Identifier("blahaj", "blahaj");
+		BLAHAJ_ID = new Identifier("blahaj", "blue_shark");
 		BREAD_ID = new Identifier("blahaj", "bread");
 	}
 
