b3acedd08d6723c9e0340ae8cf67d051410910e0
Hibi <69405847+Hibiii@users.noreply.github.com>
2022-07-24 21:28:25 -0300

    Add default tooltip

--- src/main/java/hibi/blahaj/Common.java
+++ src/main/java/hibi/blahaj/Common.java
@@ -1,13 +1,19 @@
 package hibi.blahaj;
 
+import java.util.List;
+
 import org.quiltmc.loader.api.ModContainer;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 
 public class Common {
 
@@ -15,8 +21,8 @@ public class Common {
 	public static final Identifier BREAD_ID;
 
 	public void onInitialize(ModContainer mod) {
-		Registry.register(Registry.ITEM, BLAHAJ_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC)));
-		Registry.register(Registry.ITEM, BREAD_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC)));
+		Registry.register(Registry.ITEM, BLAHAJ_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC), "item.blahaj.blue_shark.tooltip"));
+		Registry.register(Registry.ITEM, BREAD_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC), null));
 	}
 
 	static {
@@ -25,8 +31,17 @@ public class Common {
 	}
 
 	public static class CuddlyItem extends Item {
-		public CuddlyItem(Settings settings) {
+		private final Text subtitle;
+		public CuddlyItem(Settings settings, String subtitle) {
 			super(settings);
+			this.subtitle = subtitle == null? null: Text.translatable(subtitle).formatted(Formatting.GRAY);
+		}
+
+		@Override
+		public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
+			if(this.subtitle != null) {
+				tooltip.add(this.subtitle);
+			}
 		}
 		
 		@Override
