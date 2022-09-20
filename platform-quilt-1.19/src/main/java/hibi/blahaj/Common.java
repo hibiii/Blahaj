f15ed83737f36e7d2af4dc8ae14090cf6c5d55bd
Hibi <69405847+Hibiii@users.noreply.github.com>
2022-09-20 12:31:41 -0300

    Prelim support for non-crafting Blåhaj sources

--- platform-quilt-1.19/src/main/java/hibi/blahaj/Common.java
+++ platform-quilt-1.19/src/main/java/hibi/blahaj/Common.java
@@ -1,9 +1,13 @@
 package hibi.blahaj;
 
 import org.quiltmc.loader.api.ModContainer;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -13,8 +17,37 @@ public class Common {
 	public static final Identifier BREAD_ID;
 
 	public void onInitialize(ModContainer mod) {
-		Registry.register(Registry.ITEM, BLAHAJ_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC), "item.blahaj.blue_shark.tooltip"));
+		Item blueShark = Registry.register(Registry.ITEM, BLAHAJ_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC), "item.blahaj.blue_shark.tooltip"));
 		Registry.register(Registry.ITEM, BREAD_ID, new CuddlyItem(new Item.Settings().maxCount(1).group(ItemGroup.MISC), null));
+
+		// FIXME replace with QSL
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if(!source.isBuiltin()) return;
+			if(id.equals(LootTables.STRONGHOLD_CROSSING_CHEST)
+				|| id.equals(LootTables.STRONGHOLD_CORRIDOR_CHEST)) {
+				LootPool.Builder pb = LootPool.builder()
+					.with(ItemEntry.builder(blueShark)
+						.weight(5));
+				tableBuilder.pool(pb);
+			}
+			else if(id.equals(LootTables.HERO_OF_THE_VILLAGE_SHEPHERD_GIFT_GAMEPLAY)) {
+				LootPool.Builder pb = LootPool.builder()
+					.with(ItemEntry.builder(blueShark));
+				tableBuilder.pool(pb);
+			}
+			else if(id.equals(LootTables.VILLAGE_PLAINS_CHEST)) {
+				LootPool.Builder pb = LootPool.builder()
+					.with(ItemEntry.builder(blueShark));
+				tableBuilder.pool(pb);
+			}
+			else if(id.equals(LootTables.VILLAGE_TAIGA_HOUSE_CHEST)
+				|| id.equals(LootTables.VILLAGE_SNOWY_HOUSE_CHEST)) {
+				LootPool.Builder pb = LootPool.builder()
+					.with(ItemEntry.builder(blueShark)
+						.weight(5));
+				tableBuilder.pool(pb);
+			}
+		});
 	}
 
 	static {
